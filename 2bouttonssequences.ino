feb071f3584dad066ed290f9c5e3622faca27bbf
technonerdz <technonerdzfg@gmail.com>
2016-09-20 11:41:19 -0400

    Fin des commentaires

--- 2bouttonssequences.ino
+++ 2bouttonssequences.ino
@@ -4,7 +4,7 @@ int buttonPin1= 2; // Défini que le bouton 1 est sur la pin 2
 int buttonPin2 = 3; // Défini que le bouton 2 est sur la pin 3
 int ledPin1 = 13; // Défini que la led est sur la pin 13
 
-void setup()
+void setup() // initialisation
 {
  pinMode(buttonPin1, INPUT); // C'est une pin de boutton donc c'est un input
  pinMode(buttonPin2, INPUT); // C'est une pin de boutton donc c'est un input
@@ -13,22 +13,22 @@ void setup()
 }
 
 
-void loop(){
+void loop(){ // Boucle infini
  int buttonState1 = digitalRead(buttonPin1); // écris l'état des bouttons dans des variables
  int buttonState2 = digitalRead(buttonPin2); // écris l'état des bouttons dans des variables
  
- if (buttonState1 == HIGH){
-   digitalWrite(ledPin1, HIGH);
-   delay(100);
-   digitalWrite(ledPin1, LOW);
-   delay(100);
+ if (buttonState1 == HIGH){ // Si on appuie sur le boutton 1
+   digitalWrite(ledPin1, HIGH); // allume la led
+   delay(100); // attend 100 ms
+   digitalWrite(ledPin1, LOW); // éteint la led
+   delay(100); // attend 100 ms
  }
  
- if (buttonState2 == HIGH){
-   digitalWrite(ledPin1, HIGH);
-   delay(700);
-   digitalWrite(ledPin1, LOW);
-   delay(700);
+ if (buttonState2 == HIGH){ // Si on appuie sur le boutton 2
+   digitalWrite(ledPin1, HIGH); // allume la led
+   delay(700); // attend 700 ms
+   digitalWrite(ledPin1, LOW); // éteint la led
+   delay(700); // attend 700 ms
  }
  
  }
