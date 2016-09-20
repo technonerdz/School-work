436e8a67d884fdae62e4417538a640f6fc798dd9
technonerdz <technonerdzfg@gmail.com>
2016-09-20 11:59:43 -0400

    D'autres corrections

--- 2bouttonssequences.ino
+++ 2bouttonssequences.ino
@@ -1,34 +1,34 @@
 //Utiliser 2 boutons pour créer des séquences
 
-int buttonPin1= 2; // Défini que le bouton 1 est sur la pin 2
-int buttonPin2 = 3; // Défini que le bouton 2 est sur la pin 3
-int ledPin1 = 13; // Défini que la led est sur la pin 13
+int buttonPin1= 2; // Défini l'emplacement du bouton 1 sur la pin 2
+int buttonPin2 = 3; // Défini l'emplacement du bouton 2 sur la pin 3
+int ledPin1 = 13; // Défini l'emplacement de la led sur la pin 13
 
-void setup() // initialisation
+void setup() // Initialisation
 {
- pinMode(buttonPin1, INPUT); // C'est une pin de bouton donc c'est un input
- pinMode(buttonPin2, INPUT); // C'est une pin de bouton donc c'est un input
+ pinMode(buttonPin1, INPUT); // C'est une pin de bouton : input
+ pinMode(buttonPin2, INPUT); // C'est une pin de bouton : input
  pinMode(ledPin1, OUTPUT); // C'est une pin de led donc c'est un output
  delay(0); 
 }
 
 
-void loop(){ // Boucle infini
- int buttonState1 = digitalRead(buttonPin1); // écris l'état des boutons dans des variables
- int buttonState2 = digitalRead(buttonPin2); // écris l'état des boutons dans des variables
+void loop(){ // Boucle infinie
+ int buttonState1 = digitalRead(buttonPin1); // Définis un alias pour faciliter l'écriture du code (pas obligatoire)
+ int buttonState2 = digitalRead(buttonPin2); // Définis un alias pour faciliter l'écriture du code (pas obligatoire)
  
- if (buttonState1 == HIGH){ // Si on appuie sur le boutton 1
-   digitalWrite(ledPin1, HIGH); // allume la led
-   delay(100); // attend 100 ms
-   digitalWrite(ledPin1, LOW); // éteint la led
-   delay(100); // attend 100 ms
+ if (buttonState1 == HIGH){ // Si le bouton 1 est enfoncé
+   digitalWrite(ledPin1, HIGH); // Allume la led
+   delay(100); // Attend 100 ms
+   digitalWrite(ledPin1, LOW); // Éteint la led
+   delay(100); // Attend 100 ms
  }
  
- if (buttonState2 == HIGH){ // Si on appuie sur le boutton 2
-   digitalWrite(ledPin1, HIGH); // allume la led
-   delay(700); // attend 700 ms
-   digitalWrite(ledPin1, LOW); // éteint la led
-   delay(700); // attend 700 ms
+ if (buttonState2 == HIGH){ // Si le bouton 2 est enfoncé
+   digitalWrite(ledPin1, HIGH); // Allume la led
+   delay(700); // Attend 700 ms
+   digitalWrite(ledPin1, LOW); // Éteint la led
+   delay(700); // Attend 700 ms
  }
  
  }
